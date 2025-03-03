1e1a2b5027b432db48365daa5709162c276715de
Aminul Islam <aminulislam2020@gmail.com>
2025-03-04 01:47:04 +0600

    deposit amount function added

--- project.js
+++ project.js
@@ -1,2 +1,21 @@
-var msg = 'Hello World';
-console.log(msg);
+//Creating deposit function
+
+const prompt = require("prompt-sync")();
+
+const deposit = () =>{
+    while(true){
+    const depositAmount = prompt("Enter Depost Amount: ")
+    const numberDepostAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepostAmount) || numberDepostAmount<=0) {
+        console.log("Invalid Deposit Amount, Try Entering a Valid Deposit Number");
+    } else{
+    return numberDepostAmount;
+  }
+ }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
+
+
