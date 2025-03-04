a881c89c12f4af5a5eaa6c56546421e72f0adb83
Aminul Islam <aminulislam2020@gmail.com>
2025-03-05 01:29:50 +0600

    setting up bet fuction added

--- project.js
+++ project.js
@@ -30,9 +30,26 @@ const getNumberOfLines = () => {
      }
 }
 
+//setting up the bet
+
+const getBet = (balance) =>{
+    while(true){
+        const bet = prompt("Enter the total bet: ")
+        const numberOfbet = parseFloat(bet);
+    
+        if (isNaN(numberOfbet) || numberOfbet<=0 || numberOfbet > balance) {
+            console.log("Invalid bet, try again");
+        } else{
+        return numberOfbet;
+      }
+     }
+}
+
 let depositAmount = deposit();
 
 const numberOfLines = getNumberOfLines();
+
+const bet = getBet(balance);
 
 
 
