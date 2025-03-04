032f12d6ab778ef19f92bf7a96745fe1832b6ab8
Aminul Islam <aminulislam2020@gmail.com>
2025-03-04 15:12:49 +0600

    number of lines for playing function added

--- project.js
+++ project.js
@@ -15,7 +15,24 @@ const deposit = () =>{
  }
 };
 
-const depositAmount = deposit();
-console.log(depositAmount);
+//Get number of lines for playing
+
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter number of lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines<=0 || numberOfLines >3) {
+            console.log("Invalid number of lines, try again");
+        } else{
+        return numberOfLines;
+      }
+     }
+}
+
+let depositAmount = deposit();
+
+const numberOfLines = getNumberOfLines();
+
 
 
